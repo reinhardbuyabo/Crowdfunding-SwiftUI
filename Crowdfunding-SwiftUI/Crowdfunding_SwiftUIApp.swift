81654c823e81b6d76b1630e658dfe8689a62e4c9
reinhardbuyabo <buyabor@gmail.com>
2025-03-05 14:51:04 +0300

    Initial Commit

---
+++ Crowdfunding-SwiftUI/Crowdfunding_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crowdfunding_SwiftUIApp.swift
+//  Crowdfunding-SwiftUI
+//
+//  Created by Reinhard Buyabo on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Crowdfunding_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
